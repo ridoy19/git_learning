23d45bd3be02bc3ad7afdeb1bd201ff8e3fcc1bc
Md. Jakaria Hossain Ridoy <jakaria.ridoy@nagad.com.bd>
2023-07-18 23:26:11 +0600

    featured model class Car

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
+import model.Car;
 public class Test {
     public static void main(String[] args) {
+        Car newCar = new Car();
+        newCar.setId(100L);
+        newCar.setModel("Toyota Yaris");
+        newCar.setYear(2020);
         System.out.println("Hello, you are learning git. Good Luck!");
     }
 }
